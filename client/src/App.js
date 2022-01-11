f66d4a16f52df4fa97bf80bb3a537b96b53eee62
Alonso Berríos <aberrios@altimetrik.com>
2022-01-11 20:24:52 -0300

    server post model, controller, routes; client mui installed and component folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>{"I love u Catiwi <3"}</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60"></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch">
+            <Grid item xs={12} sm={7}>
+              {/* <Posts></Posts> */}
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* <Form></Form> */}
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
